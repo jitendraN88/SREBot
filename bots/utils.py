cd639d1fa94c2c906d40a5ff57422a10e2a59fe4
Sameera Sadaqat <sameera.sadaqat@gspann.com>
2024-03-14 12:07:20 +0530

    Minor name correction

--- bots/utils.py
+++ bots/utils.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 class Utils():
 
-    def getLable(self, region_label):
-        df_lable = pd.read_csv('resources/Hostname_mapping.csv')
-        df_lable = df_lable[(df_lable['region_label']==region_label)]
-        return df_lable
+    def getLabel(self, region_label):
+        df_label = pd.read_csv('resources/Hostname_mapping.csv')
+        df_label = df_label[(df_label['region_label']==region_label)]
+        return df_label
         
-    def getRequestPath(self, region_label, lable):
+    def getRequestPath(self, region_label, label):
         df_path = pd.read_csv('resources/Hostname_mapping.csv')
-        df_path = df_path[(df_path['region_label']== region_label) & (df['label']== lable)]
+        df_path = df_path[(df_path['region_label']== region_label) & (df['label']== label)]
         return df_path
 
     # lable = getLable('North America')
