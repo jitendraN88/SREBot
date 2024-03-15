560dfc5f3bcd03fd36242304b1a408ab0ea52e69
Sameera Sadaqat <sameera.sadaqat@gspann.com>
2024-03-15 14:21:17 +0530

    Minor fix for label

--- bots/utils.py
+++ bots/utils.py
@@ -5,11 +5,11 @@ class Utils():
     def getLabel(self, region_label):
         df_label = pd.read_csv('resources/Hostname_mapping.csv')
         df_label = df_label[(df_label['region_label']==region_label)]
-        return df_label
+        return df_label['label']
         
     def getRequestPath(self, region_label, label):
         df_path = pd.read_csv('resources/Hostname_mapping.csv')
-        df_path = df_path[(df_path['region_label']== region_label) & (df['label']== label)]
+        df_path = df_path[(df_path['region_label']== region_label) & (df_path['label']== label)]
         return df_path
 
     # lable = getLable('North America')
